5cb69a1b852cfaa5e217c193d801b15b47a01b34
moreluck <luckykamshak@gmail.com>
2026-03-20 14:53:57 +0100

    feat: add Stacks mainnet contract constants to frontend config

---
+++ frontend/src/chess/stacksConstants.js
@@ -0,0 +1,18 @@
+export const STACKCHESS_DEPLOYER = "SP34MN3DMM07BNAWYJSHTS4B08T8JRVK8AT810X1B";
+
+export const CONTRACTS = {
+  TRAIT: `${STACKCHESS_DEPLOYER}.sip-010-trait-ft-standard`,
+  TOKEN: `${STACKCHESS_DEPLOYER}.stackchess-token`,
+  GAME: `${STACKCHESS_DEPLOYER}.stackchess`,
+};
+
+export const GAME_STATUS = {
+  WAITING: 0,
+  ONGOING: 1,
+  WHITE_WINS: 2,
+  BLACK_WINS: 3,
+  DRAW: 4,
+  CANCELLED: 5,
+};
+
+export const NETWORK = 'mainnet';

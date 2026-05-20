3beae0c7fd1a49317e0c2be7ad4f2ca39c62540b
shaaibu7 <shaaibusuleiman9@gmail.com>
2026-05-20 14:16:22 +0100

    feat: add simulation configuration constants

--- src/simulation.ts
+++ src/simulation.ts
@@ -11,3 +11,10 @@ import { StacksMainnet } from "@stacks/network";
 import { CHESSXU_DEPLOYER, CONTRACTS } from "./index";
 
 const network = new StacksMainnet();
+
+const SIMULATION_CONFIG = {
+  ACCOUNT_COUNT: 50,
+  TRANSFER_AMOUNT: 1000000, // 1 CHESS (assuming 6 decimals)
+  RETRY_ATTEMPTS: 3,
+  POLLING_INTERVAL: 10000, // 10 seconds
+};

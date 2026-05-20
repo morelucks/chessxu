822a6706866fd6e140b9abf0b8bd755355b124fc
morelucks <luckykamshak@gmail.com>
2026-05-20 14:56:11 +0100

    feat(shop): add base shop page component structure

---
+++ frontend/src/components/pages/ShopPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ShopPage.css';
+
+export default function ShopPage() {
+  return (
+    <div className="shop-root">
+      <h1>Shop Page Coming Soon</h1>
+    </div>
+  );
+}

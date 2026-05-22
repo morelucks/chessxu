b96473c67391434c98ff82545771e6aeb093eb56
shaaibu7 <shaaibusuleiman9@gmail.com>
2026-05-22 08:00:55 +0100

    feat(ui): create basic GaslessBadge component

---
+++ frontend/src/components/ui/GaslessBadge.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GaslessBadge: React.FC = () => {
+  return (
+    <div className="gasless-badge">
+      <span>⛽ Gasless</span>
+    </div>
+  );
+};
+
+export default GaslessBadge;

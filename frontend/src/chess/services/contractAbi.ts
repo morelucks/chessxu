2682a66e0419e1f7ac218b756bfd4ddcf31027ce
shaaibu7 <shaaibusuleiman9@gmail.com>
2026-05-22 08:58:45 +0100

    feat(abi): add isTrustedForwarder function to ABI

--- frontend/src/chess/services/contractAbi.ts
+++ frontend/src/chess/services/contractAbi.ts
@@ -146,6 +146,25 @@ export const CHESSXU_ABI = [
     "type": "function"
   },
   {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "forwarder",
+        "type": "address"
+      }
+    ],
+    "name": "isTrustedForwarder",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
     "inputs": [],
     "name": "getLastGameId",
     "outputs": [

f7dd23bafe0c60fbb9e3d4d84fb96593b37d1f20
DevSolex <devsolex6@gmail.com>
2026-05-25 15:34:02 +0100

    constants: add database constants

---
+++ frontend/src/constants/db.ts
@@ -0,0 +1,15 @@
+/**
+ * Database Constants
+ */
+
+export const DB_NAME = 'ChessxuGameHistory';
+export const DB_VERSION = 1;
+export const STORES = {
+  GAMES: 'games',
+  METADATA: 'metadata'
+};
+export const INDEXES = {
+  PLAYER: 'playerIndex',
+  TIMESTAMP: 'timestampIndex',
+  CHAIN: 'chainIndex'
+};

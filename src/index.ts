2c3d0522d7497ef450ae193058bca90ca7213948
rindicomfort <kwarpojonathanrindi@gmail.com>
2026-05-29 14:54:42 +0100

    test(sdk): remove redundant code for global options

--- src/index.ts
+++ src/index.ts
@@ -1030,3 +1030,4 @@ export async function getGlobalStats(network: StacksNetwork = defaultNetwork): P
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
+// commit reference tracking checkpoint

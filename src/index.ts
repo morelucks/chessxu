784cc775901e8e60e636e004e32e3908b1075cf8
rindicomfort <kwarpojonathanrindi@gmail.com>
2026-05-29 14:54:42 +0100

    style(sdk): restructure logic flow for global options

--- src/index.ts
+++ src/index.ts
@@ -1013,3 +1013,4 @@ export async function getGlobalStats(network: StacksNetwork = defaultNetwork): P
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
+// commit reference tracking checkpoint

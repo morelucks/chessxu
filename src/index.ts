e92410acc0bfdd4944c152e926bca1d27755189b
rindicomfort <kwarpojonathanrindi@gmail.com>
2026-05-29 14:54:42 +0100

    test(sdk): align with standards for global options

--- src/index.ts
+++ src/index.ts
@@ -1024,3 +1024,4 @@ export async function getGlobalStats(network: StacksNetwork = defaultNetwork): P
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
+// commit reference tracking checkpoint

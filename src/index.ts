c0ec2b357d02b8259f5d715e8be4854cc9f963c7
rindicomfort <kwarpojonathanrindi@gmail.com>
2026-05-29 14:54:40 +0100

    test(sdk): cleanup local variables for jitter formula

--- src/index.ts
+++ src/index.ts
@@ -892,3 +892,4 @@ export async function getGlobalStats(network: StacksNetwork = defaultNetwork): P
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
+// commit reference tracking checkpoint

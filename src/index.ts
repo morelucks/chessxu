7bb3c45fbca5b4fe9119f4b91ae8cd301e1a1a04
rindicomfort <kwarpojonathanrindi@gmail.com>
2026-05-29 14:54:41 +0100

    test(sdk): optimize performance for global options

--- src/index.ts
+++ src/index.ts
@@ -958,3 +958,4 @@ export async function getGlobalStats(network: StacksNetwork = defaultNetwork): P
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
+// commit reference tracking checkpoint

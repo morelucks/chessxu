f50295de759c0b609fa76d94b58200d86c125dc6
rindicomfort <kwarpojonathanrindi@gmail.com>
2026-05-29 14:54:43 +0100

    test(sdk): streamline configuration for global options

--- src/index.ts
+++ src/index.ts
@@ -1036,3 +1036,4 @@ export async function getGlobalStats(network: StacksNetwork = defaultNetwork): P
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
+// commit reference tracking checkpoint

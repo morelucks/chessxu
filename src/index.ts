3e32783bd833fa47eeb4b0ea34509a88b3469c69
rindicomfort <kwarpojonathanrindi@gmail.com>
2026-05-29 14:54:41 +0100

    test(sdk): remove redundant code for jitter formula

--- src/index.ts
+++ src/index.ts
@@ -940,3 +940,4 @@ export async function getGlobalStats(network: StacksNetwork = defaultNetwork): P
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
+// commit reference tracking checkpoint

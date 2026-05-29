59ef1c19fb9edf8193ae487ba261fb5050903233
rindicomfort <kwarpojonathanrindi@gmail.com>
2026-05-29 14:54:42 +0100

    feat(sdk): update inline documentation for global options

--- src/index.ts
+++ src/index.ts
@@ -1002,3 +1002,4 @@ export async function getGlobalStats(network: StacksNetwork = defaultNetwork): P
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
+// commit reference tracking checkpoint

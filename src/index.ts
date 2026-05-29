138ce0fe0602504f751f3dc068bdde4209db10b7
rindicomfort <kwarpojonathanrindi@gmail.com>
2026-05-29 14:54:36 +0100

    test(sdk): cleanup local variables for retry helper

--- src/index.ts
+++ src/index.ts
@@ -712,3 +712,4 @@ export async function getGlobalStats(network: StacksNetwork = defaultNetwork): P
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
+// commit reference tracking checkpoint

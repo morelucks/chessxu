536e67fed326af20c0e07c173cfa70da22d80c6c
rindicomfort <kwarpojonathanrindi@gmail.com>
2026-05-29 14:54:42 +0100

    refactor(sdk): simplify expressions for global options
    docs(sdk): simplify expressions for global options
    
    test(sdk): simplify expressions for global options

--- src/index.ts
+++ src/index.ts
@@ -1016,3 +1016,6 @@ export async function getGlobalStats(network: StacksNetwork = defaultNetwork): P
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
+// commit reference tracking checkpoint
+// commit reference tracking checkpoint
+// commit reference tracking checkpoint

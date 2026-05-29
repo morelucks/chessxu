11df88cba65bf7bb5e0ea5d6919f88c4a4fff676
rindicomfort <kwarpojonathanrindi@gmail.com>
2026-05-29 14:54:43 +0100

    style(sdk): optimize performance for getLastGameId query

--- src/index.ts
+++ src/index.ts
@@ -1049,3 +1049,4 @@ export async function getGlobalStats(network: StacksNetwork = defaultNetwork): P
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
+// commit reference tracking checkpoint

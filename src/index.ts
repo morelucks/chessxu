f849cce632bd3052090b679f0de177e822b78ee8
rindicomfort <kwarpojonathanrindi@gmail.com>
2026-05-29 14:54:42 +0100

    style(sdk): cleanup local variables for global options

--- src/index.ts
+++ src/index.ts
@@ -983,3 +983,4 @@ export async function getGlobalStats(network: StacksNetwork = defaultNetwork): P
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
+// commit reference tracking checkpoint

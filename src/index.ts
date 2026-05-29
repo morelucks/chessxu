7e6e9ae2182a703413128ca89aa27d447d3da7ca
rindicomfort <kwarpojonathanrindi@gmail.com>
2026-05-29 14:54:42 +0100

    chore(sdk): standardize formatting for global options

--- src/index.ts
+++ src/index.ts
@@ -985,3 +985,4 @@ export async function getGlobalStats(network: StacksNetwork = defaultNetwork): P
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
+// commit reference tracking checkpoint

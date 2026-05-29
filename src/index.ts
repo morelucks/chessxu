946189b6c599ce85493b3a43a7bf62774fa4876d
rindicomfort <kwarpojonathanrindi@gmail.com>
2026-05-29 14:54:37 +0100

    test(sdk): cleanup local variables for backoff logic

--- src/index.ts
+++ src/index.ts
@@ -802,3 +802,4 @@ export async function getGlobalStats(network: StacksNetwork = defaultNetwork): P
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
+// commit reference tracking checkpoint

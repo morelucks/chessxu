087721de095dafb44bef0ef820f1fa7da5c35435
rindicomfort <kwarpojonathanrindi@gmail.com>
2026-05-29 14:54:39 +0100

    test(sdk): remove redundant code for backoff logic

--- src/index.ts
+++ src/index.ts
@@ -850,3 +850,4 @@ export async function getGlobalStats(network: StacksNetwork = defaultNetwork): P
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
+// commit reference tracking checkpoint

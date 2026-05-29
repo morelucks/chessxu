95acdfdff706f2d26be25003d46f92ab743a344a
rindicomfort <kwarpojonathanrindi@gmail.com>
2026-05-29 14:54:38 +0100

    style(sdk): restructure logic flow for backoff logic

--- src/index.ts
+++ src/index.ts
@@ -833,3 +833,4 @@ export async function getGlobalStats(network: StacksNetwork = defaultNetwork): P
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
 // commit reference tracking checkpoint
+// commit reference tracking checkpoint

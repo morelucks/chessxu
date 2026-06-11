c89858eb574972de2c9a79730909131985060116
morelucks <luckykamshak@gmail.com>
2026-06-11 22:42:39 +0100

    test: update to ESM import syntax

--- celo-contract/test/ChessxuV2.test.js
+++ celo-contract/test/ChessxuV2.test.js
@@ -320,3 +320,5 @@ describe("ChessxuV2 - ERC-20 Wager Refund on Draw/Cancellation", function () {
             await expect(
                 chessxuV2.connect(owner).resolveGame(1, 0)
             ).to.be.revertedWithCustomError(chessxuV2, "InvalidStatus");
+
+            await expect(

909b4e1d364e3432274bb744668c2732942df3e9
morelucks <luckykamshak@gmail.com>
2026-06-11 22:42:38 +0100

    test: add wager deduction verification in cancel

--- celo-contract/test/ChessxuV2.test.js
+++ celo-contract/test/ChessxuV2.test.js
@@ -292,3 +292,5 @@ describe("ChessxuV2 - ERC-20 Wager Refund on Draw/Cancellation", function () {
         it("should revert if non-owner tries to resolve the game", async function () {
             const { chessxuV2, mockToken, player1, player2 } = await deployFixture();
 
+            const contractAddr = await chessxuV2.getAddress();
+

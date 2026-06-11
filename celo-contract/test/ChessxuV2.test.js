58820e8f90e4b5436ed2caee6c367ab48ab4bb5e
morelucks <luckykamshak@gmail.com>
2026-06-11 22:42:38 +0100

    test: assert status 1 reverts

--- celo-contract/test/ChessxuV2.test.js
+++ celo-contract/test/ChessxuV2.test.js
@@ -232,3 +232,6 @@ describe("ChessxuV2 - ERC-20 Wager Refund on Draw/Cancellation", function () {
 
         it("should reduce contract ERC-20 balance to zero after Cancellation refund", async function () {
             const { chessxuV2, mockToken, owner, player1 } = await deployFixture();
+
+            const contractAddr = await chessxuV2.getAddress();
+

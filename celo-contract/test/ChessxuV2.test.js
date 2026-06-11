2646b6edaa6c24754b6ed1c1688babffd803776f
morelucks <luckykamshak@gmail.com>
2026-06-11 22:42:39 +0100

    test: finalize cancellation resolution tests

--- celo-contract/test/ChessxuV2.test.js
+++ celo-contract/test/ChessxuV2.test.js
@@ -332,3 +332,5 @@ describe("ChessxuV2 - ERC-20 Wager Refund on Draw/Cancellation", function () {
         });
 
         it("should revert if resolving an already resolved game", async function () {
+            const { chessxuV2, mockToken, owner, player1, player2 } = await deployFixture();
+

6f46293a5c1a27cee5c1c225bafa5e4b33475f2d
morelucks <luckykamshak@gmail.com>
2026-06-11 22:42:39 +0100

    test: refine describe block naming

--- celo-contract/test/ChessxuV2.test.js
+++ celo-contract/test/ChessxuV2.test.js
@@ -324,3 +324,5 @@ describe("ChessxuV2 - ERC-20 Wager Refund on Draw/Cancellation", function () {
             await expect(
                 chessxuV2.connect(owner).resolveGame(1, 1)
             ).to.be.revertedWithCustomError(chessxuV2, "InvalidStatus");
+
+            // Status 6+ are invalid

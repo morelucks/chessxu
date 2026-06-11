5db1b581d06687969cac7620906c70a086a13788
morelucks <luckykamshak@gmail.com>
2026-06-11 22:42:39 +0100

    test: ensure consistent code formatting

--- celo-contract/test/ChessxuV2.test.js
+++ celo-contract/test/ChessxuV2.test.js
@@ -340,3 +340,5 @@ describe("ChessxuV2 - ERC-20 Wager Refund on Draw/Cancellation", function () {
             await mockToken.connect(player1).approve(contractAddr, WAGER_AMOUNT);
             await chessxuV2.connect(player1).createGame(WAGER_AMOUNT, false);
             await mockToken.connect(player2).approve(contractAddr, WAGER_AMOUNT);
+            await chessxuV2.connect(player2).joinGame(1);
+

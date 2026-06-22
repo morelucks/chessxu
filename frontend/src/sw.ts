c2fc85c8684af7345baeb4bcaa966e20bfcddac1
morelucks <luckykamshak@gmail.com>
2026-06-22 23:08:02 +0100

    feat(cache): implement activate event

--- frontend/src/sw.ts
+++ frontend/src/sw.ts
@@ -120,3 +120,4 @@ self.addEventListener('fetch', (event: any) => {
         })
         .catch(() => {
           return caches.match(request).then((cachedResponse) => {
+            if (cachedResponse) {

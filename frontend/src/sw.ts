d709cc8a0d5e3a0ef1c71c0ad80305cb87fff93f
morelucks <luckykamshak@gmail.com>
2026-06-22 23:08:02 +0100

    feat(cache): implement network-first strategy

--- frontend/src/sw.ts
+++ frontend/src/sw.ts
@@ -123,3 +123,4 @@ self.addEventListener('fetch', (event: any) => {
             if (cachedResponse) {
               return cachedResponse;
             }
+            if (request.mode === 'navigate') {

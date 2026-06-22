fe9f55cc7440525fdee92630b9594b885903dd04
morelucks <luckykamshak@gmail.com>
2026-06-22 23:08:00 +0100

    feat(sw): intercept install event

--- frontend/src/sw.ts
+++ frontend/src/sw.ts
@@ -42,3 +42,4 @@ self.addEventListener('activate', (event: any) => {
 
 // Fetch event: serve cached assets when offline or for speed
 self.addEventListener('fetch', (event: any) => {
+  const request = event.request;

335eac3043f706c4fe4034802b445c54a8178793
morelucks <luckykamshak@gmail.com>
2026-06-22 23:08:01 +0100

    feat(sw): write API filter

--- frontend/src/sw.ts
+++ frontend/src/sw.ts
@@ -83,3 +83,4 @@ self.addEventListener('fetch', (event: any) => {
             if (networkResponse && networkResponse.status === 200) {
               caches.open(CACHE_NAME).then((cache) => cache.put(request, networkResponse));
             }
+          }).catch(() => {/* Ignore network update failure offline */});

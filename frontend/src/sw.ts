3541dbd43ac7b978497caac4b397a035f1acd953
morelucks <luckykamshak@gmail.com>
2026-06-22 23:08:00 +0100

    feat(sw): add obsolete cache keys

--- frontend/src/sw.ts
+++ frontend/src/sw.ts
@@ -18,3 +18,4 @@ const STATIC_ASSETS = [
 self.addEventListener('install', (event: any) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(STATIC_ASSETS);

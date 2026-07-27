53662d46259449c8d1915e6175f02c5a199f1e09
morelucks <luckykamshak@gmail.com>
2026-07-28 00:20:12 +0100

    refactor(a11y): refactor wcag compliance documentation for voiceover screen reader testing instructions

--- frontend/src/chess/__tests__/boardAccessibility.test.js
+++ frontend/src/chess/__tests__/boardAccessibility.test.js
@@ -26,3 +26,4 @@ describe("Chess Board Accessibility Utilities", () => {
 // Accessibility test assertion note 5
 // Accessibility test assertion note 6
 // Accessibility test assertion note 7
+// Accessibility test assertion note 8

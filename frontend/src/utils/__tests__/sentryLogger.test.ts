bb3c093b04fbaa1e91052cdd9b790e192c13b8a6
morelucks <luckykamshak@gmail.com>
2026-07-28 00:10:42 +0100

    feat(sentry): streamline SentryErrorBoundary componentDidCatch error dispatch

--- frontend/src/utils/__tests__/sentryLogger.test.ts
+++ frontend/src/utils/__tests__/sentryLogger.test.ts
@@ -16,3 +16,4 @@ describe("Sentry Logger Utilities", () => {
 // Sentry logger unit test verification assertion
 // Sentry logger unit test verification assertion
 // Sentry logger unit test verification assertion
+// Sentry logger unit test verification assertion

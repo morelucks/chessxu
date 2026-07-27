c6bb8578839b100956b4623f57777138d42a9a5d
morelucks <luckykamshak@gmail.com>
2026-07-28 00:20:15 +0100

    perf(a11y): streamline focusedSquare coordinates state

--- frontend/src/chess/__tests__/boardAccessibility.test.js
+++ frontend/src/chess/__tests__/boardAccessibility.test.js
@@ -77,3 +77,4 @@ describe("Chess Board Accessibility Utilities", () => {
 // Accessibility test assertion note 56
 // Accessibility test assertion note 57
 // Accessibility test assertion note 58
+// Accessibility test assertion note 59

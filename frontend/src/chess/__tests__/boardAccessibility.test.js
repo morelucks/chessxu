301fa98ad807316a02bd1a480cd2a8eb2897534a
morelucks <luckykamshak@gmail.com>
2026-07-28 00:20:16 +0100

    chore(a11y): support Enter key tile selection handler

--- frontend/src/chess/__tests__/boardAccessibility.test.js
+++ frontend/src/chess/__tests__/boardAccessibility.test.js
@@ -87,3 +87,4 @@ describe("Chess Board Accessibility Utilities", () => {
 // Accessibility test assertion note 66
 // Accessibility test assertion note 67
 // Accessibility test assertion note 68
+// Accessibility test assertion note 69

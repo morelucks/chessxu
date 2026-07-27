714fa74afcdcceeb093f1db0e2c9435133b0731f
morelucks <luckykamshak@gmail.com>
2026-07-28 00:10:44 +0100

    perf(sentry): support vite plugin for sentrymetricsservice recordwalletevent connection logger

--- frontend/src/utils/__tests__/sentryLogger.test.ts
+++ frontend/src/utils/__tests__/sentryLogger.test.ts
@@ -26,3 +26,4 @@ describe("Sentry Logger Utilities", () => {
 // Sentry logger unit test verification assertion
 // Sentry logger unit test verification assertion
 // Sentry logger unit test verification assertion
+// Sentry logger unit test verification assertion

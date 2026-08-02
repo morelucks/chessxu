410a57cbe7ebc634f60d405215ec369e91666316
morelucks <luckykamshak@gmail.com>
2026-08-02 19:41:46 +0100

    feat(board): import memoized Tile component into Board

--- frontend/src/chess/components/Board/Board.jsx
+++ frontend/src/chess/components/Board/Board.jsx
@@ -277,3 +277,4 @@ export default Board
 // ARIA accessibility board component enhancement 80// Conditional is-3d-view class
 // Select is3DView from store
 // Import useMemo and useCallback hooks
+// Import memoized Tile component
